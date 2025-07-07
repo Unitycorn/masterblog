6a022e29b29526c8256199584a577df6d560ac68
Unitycorn <flaig.m@gmx.de>
2025-07-07 14:36:38 +0200

    Cleanups

--- app.py
+++ app.py
@@ -5,17 +5,20 @@ app = Flask(__name__)
 
 
 def get_all_blog_posts():
+    """Loads all data from the json file"""
     with open('data.json', 'r') as handle:
         blog_posts = json.load(handle)
     return blog_posts
 
 
 def save_to_file(data):
+    """Saves data into the json file"""
     with open('data.json', 'w') as handle:
         json.dump(data, handle)
 
 
 def fetch_post_by_id(blog_id):
+    """Fetches a specific post from the json file, if not found return None"""
     blog_posts = get_all_blog_posts()
     for post in blog_posts:
         if post['id'] == blog_id:
@@ -25,6 +28,7 @@ def fetch_post_by_id(blog_id):
 
 @app.route('/')
 def index():
+    """The main page containing all posts"""
     # add code here to fetch the job posts from a file
     blog_posts = get_all_blog_posts()
     return render_template('index.html', posts=blog_posts)
@@ -32,6 +36,7 @@ def index():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    """Route to add a new post via form on a new template returns to main page after adding"""
     blog_posts = get_all_blog_posts()
     if request.method == 'POST':
         author = request.form['author']
@@ -44,11 +49,13 @@ def add():
         blog_posts.append({'id': new_blog_id, 'author': author, 'title': title, 'content': content})
         save_to_file(blog_posts)
         return redirect(url_for('index'))
+    # If method = GET, render the template to add post
     return render_template('add.html')
 
 
 @app.route('/delete/<int:post_id>')
 def delete(post_id):
+    """route to delete a post from the json file, redirects to main page"""
     blog_posts = get_all_blog_posts()
     for blog in blog_posts:
         if blog['id'] == post_id:
@@ -60,15 +67,14 @@ def delete(post_id):
 
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
 def update(post_id):
+    """Route to a form to make changes to a specific post"""
     # Fetch the blog posts from the JSON file
     blog_posts = get_all_blog_posts()
     post = fetch_post_by_id(post_id)
     if post is None:
         # Post not found
         return "Post not found", 404
-
     if request.method == 'POST':
-        pass
         # Update the post in the JSON file
         author = request.form['author']
         title = request.form['title']
